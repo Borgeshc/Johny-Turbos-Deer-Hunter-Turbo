5544f1d9ccd18f674a5bc9be337ac1776f5e2de4
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-05-05 21:29:55 -0500

    Added mouse turning, added the bullet, clamped sprites

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,6 +7,8 @@ public class Movement : MonoBehaviour
     public float speed;
     public float minClamp;
     public float maxClamp;
+    public bool keyboardTurn;
+
     float vertical;
 
     Quaternion rightRotation = new Quaternion(0,180,0,0);
@@ -15,13 +17,27 @@ public class Movement : MonoBehaviour
     {
         vertical = Input.GetAxis("Vertical");
 
-        if(Input.GetKeyDown(KeyCode.A) && transform.rotation != Quaternion.identity)
+        if(keyboardTurn)
         {
-            transform.rotation = Quaternion.identity;
+            if (Input.GetKeyDown(KeyCode.A) && transform.rotation != Quaternion.identity)
+            {
+                transform.rotation = Quaternion.identity;
+            }
+            else if (Input.GetKeyDown(KeyCode.D) && transform.rotation != rightRotation)
+            {
+                transform.rotation = rightRotation;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.D) && transform.rotation != rightRotation)
+        else
         {
-            transform.rotation = rightRotation;
+            if(Input.mousePosition.x < Screen.width / 2 && transform.rotation != Quaternion.identity)
+            {
+                transform.rotation = Quaternion.identity;
+            }
+            else if (Input.mousePosition.x > Screen.width / 2 && transform.rotation != rightRotation)
+            {
+                transform.rotation = rightRotation;
+            }
         }
         
         transform.Translate(new Vector3(0, vertical * speed * Time.deltaTime, 0));
